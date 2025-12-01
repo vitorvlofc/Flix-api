8351be9c4b9363192ea8b15ac0a8ca86ac6e9fb6
vitor <vitorvlofc@users.noreply.github.com>
2025-12-01 16:17:43 -0300

    121 - Detalhe de gênero

--- app/urls.py
+++ app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_view
+from genres.views import genre_create_list_view, genre_detail_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre'),
+    path('genres/', genre_create_list_view, name='genre-create-list'),
+    path('genres/<int:pk>/', genre_detail_view, name='genre-detail-view'),
 ]
