95a6e5315cc24512cf9bc022054c2722afa93a90
vitor <vitorvlofc@users.noreply.github.com>
2025-12-11 20:27:11 -0300

    125 - Sobre generic views do DRF

--- genres/views.py
+++ genres/views.py
@@ -3,6 +3,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from genres.models import Genre
+from rest_framework import generics
 
 @csrf_exempt
 def genre_create_list_view(request):
